d8eb743b983ba3195c0a9e114f6e05cad472fd0e
WarFox (Deepu Mohan Puthrote) <git@deepumohan.com>
2021-05-23 21:11:45 +0100

    Github -> github.com for emacs-profiles.el

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,8 +1,8 @@
-(("default" . ((user-emacs-directory . "~/Workspace/GitHub/hlissner/doom-emacs")
+(("default" . ((user-emacs-directory . "~/Workspace/github.com/hlissner/doom-emacs")
                (env . (("DOOMDIR" . "~/.doom.d")))))
 
- ("spacemacs" . ((user-emacs-directory . "~/Workspace/GitHub/syl20bnr/spacemacs")
+ ("spacemacs" . ((user-emacs-directory . "~/Workspace/github.com/syl20bnr/spacemacs")
                          (env . (("SPACEMACSDIR" . "~/.spacemacs.d")))))
 
- ("doom" . ((user-emacs-directory . "~/Workspace/GitHub/hlissner/doom-emacs")
+ ("doom" . ((user-emacs-directory . "~/Workspace/github.com/hlissner/doom-emacs")
             (env . (("DOOMDIR" . "~/.doom.d"))))))
