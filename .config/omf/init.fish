daee3c83b10bc2b8ba0702d2c2d6c8796c65acfb
Deepu Mohan Puthrote <deepu.puthrote@fundingcircle.com>
2021-05-27 08:24:32 +0100

    remove omf :knife:; we have starship and fisher :tada:!

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -1,19 +1,23 @@
-# rbenv
-status --is-interactive; and source (rbenv init -|psub)
+# ==> libflags
+# set dir (dirname (status -f))
+# source $dir/libflags.fish
 
+# rbenv
+# status --is-interactive; and source (rbenv init -|psub)
 
 # ==> pyenv-virtualenv
 # To enable auto-activation add to your profile:
 status --is-interactive; and pyenv init - | source
 status --is-interactive; and pyenv virtualenv-init - | source
 
-
-# ==> libflags
-set dir (dirname (status -f))
-source $dir/libflags.fish
+# ==> jenv
+# status --is-interactive; and source (jenv init -|psub)
 
 # blast off with starship
 starship init fish | source
 
+# direnv hook
+eval (direnv hook fish)
+
 # ==> local settings and overrides
-source $dir/init.local.fish
+# source $dir/init.local.fish
